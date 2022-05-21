f8ed9380d45b9698c430f85fb7052c1f022b7aa9
PauloGabriel347 <paulogabriel347@gmail.com>
2022-05-20 21:59:31 -0300

    1 file modified, 1 added

--- 0.4.3/index.js
+++ 0.4.3/index.js
@@ -1,3 +1,28 @@
-// run `node index.js` in the terminal
+const arquivos = [
+  {
+    id: 1,
+    nome: 'Meu Trabalho v1',
+    excluido: false,
+  },
+  {
+    id: 2,
+    nome: 'Meu Trabalho v2',
+    excluido: false,
+  },
+  {
+    id: 3,
+    nome: 'Meu Trabalho v3',
+    excluido: false,
+  },
+  {
+    id: 4,
+    nome: 'Meu Trabalho v4',
+    excluido: true,
+  },
+];
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+arquivos.forEach((arquivos) => {
+  arquivos.excluido = !arquivos.excluido;
+});
+
+console.log(arquivos);
