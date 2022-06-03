a512f576c79d56a351d6f64c890d5b82a9ac1549
PauloGabriel347 <paulogabriel347@gmail.com>
2022-06-03 08:59:18 -0300

    1 file added

---
+++ 0.1.3/index.js
@@ -0,0 +1,21 @@
+const anos = ['2012', '2010', '2020', '2022', '2035', '2045'];
+let anosAnteriores = [];
+let anosMaiores = [];
+
+function menorAnoPassado() {
+  for (let i = 0; i < anos.length; i++) {
+    if (anos[i] <= 2021) {
+      anosAnteriores.push(anos[i]);
+    }
+  }
+  return anosAnteriores;
+}
+
+function maiorAnoPassado() {
+  for (let i = 0; i < anos.length; i++) {
+    if (anos[i] >= 2021) {
+      anosMaiores.push(anos[i]);
+    }
+  }
+  return anosMaiores;
+}
