d99a0b9467947ecab976c96a3bee70278b82c33e
PauloGabriel347 <paulogabriel347@gmail.com>
2022-06-24 20:53:24 -0300

    1 file modified

--- 1.7.2/App.js
+++ 1.7.2/App.js
@@ -1,25 +1,25 @@
-const Personagens = () => {};
-let Familia = "Redfield";
-
-export default function App() {
+export const Personagens = (props) => {
   const personagens = [
-    "Jill Valentine",
-    "Claire Redfield",
-    "Ada Wong",
-    "Chris Redfield",
-    "Leon S. Kennedy",
-    "Albert Wesker"
+    'Jill Valentine',
+    'Claire Redfield',
+    'Ada Wong',
+    'Chris Redfield',
+    'Leon S. Kennedy',
+    'Albert Wesker',
   ];
+
+  const filtrarPersonagem = (personagem) => {
+    const familiaUpper = props.familia.toUppercase();
+    return personagem.toUppercase().includes(familiaUpper);
+  };
+
+  const mapearPersonagem = (personagem) => <li>{personagem}</li>;
+
   return (
     <div>
       <h3>Personagens de Resident Evil</h3>
-      <ul>
-        {personagens
-          .filter((personagem) => personagem.includes(Familia))
-          .map((personagem) => (
-            <li>{personagem}</li>
-          ))}
-      </ul>
+      <ul>{personagens.filter(filtrarPersonagem).map(mapearPersonagem)}</ul>
     </div>
   );
-}
+};
+export default App;
